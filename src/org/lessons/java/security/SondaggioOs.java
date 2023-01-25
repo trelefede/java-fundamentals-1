5a1e550b5b53ecbb182cc000840b210a1ae1d83d
TreleaniFederico <treleani.federico@gmail.com>
2023-01-25 16:03:46 +0100

    add casting float int

--- src/org/lessons/java/security/SondaggioOs.java
+++ src/org/lessons/java/security/SondaggioOs.java
@@ -3,13 +3,22 @@ package org.lessons.java.security;
 public class SondaggioOs {
 		public static void main(String[] args) {
 			
-			int totalStudents = 190;
-			int linuxUsers = ((43*100)/totalStudents);
-			int osxUsers = ((67*100)/totalStudents);
-			int windowsUsers = ((80*100)/totalStudents);
+			float totalStudents = 190;
+			float linuxUsers = ((43/totalStudents)*100);
+			float osxUsers = ((67/totalStudents)*100);
+			float windowsUsers = ((80/totalStudents)*100);
 			
-			System.out.println("Utenti linux:" + linuxUsers + "%");
-			System.out.println("Utenti osx:" + osxUsers + "%");
-			System.out.println("Utenti windows:" + windowsUsers + "%");
+			System.out.println("Totale studenti intervistati: " + totalStudents);
+			System.out.println("Utenti linux: " + linuxUsers + "%");
+			System.out.println("Utenti osx: " + osxUsers + "%");
+			System.out.println("Utenti windows: " + windowsUsers + "%"); //senza casting per vedere la differenza in console
+			
+			System.out.println("-------------------------------------------------------------");
+			
+			//con casting da float ad int
+			System.out.println("Totale studenti intervistati (casting int): " + (int)totalStudents);
+			System.out.println("Utenti linux: " + (int)linuxUsers + "%");
+			System.out.println("Utenti osx: " + (int)osxUsers + "%");
+			System.out.println("Utenti windows: " + (int)windowsUsers + "%");
 		}
 }
