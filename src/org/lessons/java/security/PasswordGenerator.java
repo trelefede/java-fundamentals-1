884888a02cf443195594e50a24c323a64fe0a88c
TreleaniFederico <treleani.federico@gmail.com>
2023-01-25 16:43:58 +0100

    add scanner class + add user input

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,11 +1,19 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 		public static void main(String[] args) {
 			
-			String nome = "Federico";
-			String cognome = "Treleani";
-			String colorePrefe = "Rosso";
+			Scanner scannerObj = new Scanner(System.in);
+			
+			System.out.println("Inserisci il nome:");
+			String nome = scannerObj.nextLine();
+			
+			System.out.println("Inserisci il cognome:");
+			String cognome = scannerObj.nextLine();
+			
+			String colorePrefe = "rosso";
 			int giorno = 28;
 			int mese = 9;
 			int anno = 1994;
